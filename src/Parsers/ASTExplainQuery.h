1b61b7ac1236fbd3f5cb5720911f555fb231423c
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-06-23 16:12:03 +0300

    Fix formatting.

--- src/Parsers/ASTExplainQuery.h
+++ src/Parsers/ASTExplainQuery.h
@@ -54,6 +54,7 @@ protected:
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << toString(kind, old_syntax) << (settings.hilite ? hilite_none : "") << " ";
         ast_settings->formatImpl(settings, state, frame);
+        settings.ostr << settings.nl_or_ws;
         query->formatImpl(settings, state, frame);
     }
 
